d3db9e8af80192f3f99682c1a41eb44f844a5002
Ethan Ji <ethanji1000@gmail.com>
2019-02-26 23:00:57 -0800

    add more question

--- linear/1_twoSum.java
+++ linear/1_twoSum.java
@@ -1,8 +1,66 @@
+/* Solution 1: 
+first idea --> brute force always:
+use two for loop to through the each elements in the array: 
+instead of thinking of nums[i] + nums[j] == target; using 
+nums[j] == target - nums[i]
+*/
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] == target - nums[i]) {
+                    return new int[] {i, j};
+                }
+            }
+        }
+        throw new IllegalArgumentException ("no such solution");
+    }
+}
+
+// Time complexity: O(n^2) ; Space complexity: O(1)
+
+
+/*Solution2: 
+How to reduce the time complexity? Use space to reduce time --> hashmap
+while iterate though the loop and insert all the elements in the map; 
+we can look back to see if the current value already in the map; */
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            //while loop though and insert all the elements in the map
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
+
+//Time complexity O(n^2) ; Space complexity: O(n)
+
+
+/* Solution3:
+If we have still want to reduce time complexity, how to improve? 
+Normally,time complexity less than O(n) is log(n);
+--> can we sort the array and then use binary search? 
+step1: create a length 2 array to store the results; (corer cases)
+step2: copy the original array 
+step3: use binary search to find the two nums
+step4: go back to original array, find the original associated indexes; 
+
+*/
+
 class Solution {
     
     public int[] twoSum(int[] nums, int target) {
         
         int[] res = new int[2];
+        //corner case 
         if (nums == null || nums.length < 2) {
             return res;
         }
@@ -40,3 +98,14 @@ class Solution {
         
     }
 }
+
+// Time Complexity Olog(n); (Sort complexity is log(n), binary search also log(n); one mistake above)
+// Sapce Complexity: O(n)
+
+
+
+
+
+
+
+
